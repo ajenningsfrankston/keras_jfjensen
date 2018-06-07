35d7397022f782eea4565516daccf8220e62fe48
Andrew Jennings <ajenningsfrankston@gmail.com>
2018-06-07 20:49:15 +1000

    start

--- keras_jfj.py
+++ keras_jfj.py
@@ -9,3 +9,4 @@ import pandas as pd
 training_data = pd.read_csv('~/numerai_datasets/numerai_training_data.csv', header=0)
 tournament_data = pd.read_csv('~/numerai_datasets/numerai_tournament_data.csv', header=0)
 
+xx
