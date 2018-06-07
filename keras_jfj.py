3d5d9cac7d36a454775da6b23443a5aa1eba8d17
Andrew Jennings <ajenningsfrankston@gmail.com>
2018-06-07 18:35:02 +1000

    start

---
+++ keras_jfj.py
@@ -0,0 +1,11 @@
+#
+# keras example from: https://github.com/jfjensen/numerai_article/blob/master/NumeraiArticle.ipynb
+
+
+import numpy as np
+import pandas as pd
+
+
+training_data = pd.read_csv('~/numerai_data/numerai_training_data.csv', header=0)
+tournament_data = pd.read_csv('~/numerai_data/numerai_tournament_data.csv', header=0)
+
