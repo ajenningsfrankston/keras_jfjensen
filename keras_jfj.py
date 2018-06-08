606b5fa135f5525848d96b14628c6ae0e2d58632
Andrew Jennings <ajenningsfrankston@gmail.com>
2018-06-08 16:49:41 +1000

    first model

--- keras_jfj.py
+++ keras_jfj.py
@@ -26,5 +26,31 @@ from keras.models import Sequential
 from keras.layers import Dense, BatchNormalization, Dropout, Activation
 from keras.wrappers.scikit_learn import KerasClassifier
 
+def create_model(neurons=200, dropout=0.2):
+    model = Sequential()
+    model.add(Dense(neurons, input_shape=(50,), kernel_initializer='glorot_uniform', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Dropout(dropout))
+    model.add(Activation('relu'))
+    model.add(Dense(1, activation='sigmoid', kernel_initializer='glorot_normal'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['binary_crossentropy', 'accuracy'])
+    return model
 
-xxx
+model = KerasClassifier(build_fn=create_model, epochs=8, batch_size=128, verbose=0)
+
+neurons = [8, 16]
+dropout = [0.01, 0.4]
+param_grid = dict(neurons=neurons, dropout=dropout)
+
+gkf = GroupKFold(n_splits=5)
+kfold_split = gkf.split(X, Y, groups=complete_training_data.era)
+
+grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=kfold_split, scoring='neg_log_loss',n_jobs=1, verbose=3)
+grid_result = grid.fit(X.values, Y.values)
+
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
