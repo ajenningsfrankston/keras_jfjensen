284578b759b70b0fe8966f6a52862bfcd15a44d8
Andrew Jennings <ajenningsfrankston@gmail.com>
2018-06-08 15:18:58 +1000

    start

--- keras_jfj.py
+++ keras_jfj.py
@@ -9,4 +9,21 @@ import pandas as pd
 training_data = pd.read_csv('~/numerai_datasets/numerai_training_data.csv', header=0)
 tournament_data = pd.read_csv('~/numerai_datasets/numerai_tournament_data.csv', header=0)
 
-xx
+validation_data = tournament_data[tournament_data.data_type=='validation']
+complete_training_data = training_data
+
+# (note not training on validation data)
+
+features = [f for f in list(complete_training_data) if "feature" in f]
+X = complete_training_data[features]
+Y = complete_training_data["target"]
+
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GroupKFold
+from keras.models import Sequential
+from keras.layers import Dense, BatchNormalization, Dropout, Activation
+from keras.wrappers.scikit_learn import KerasClassifier
+
+
